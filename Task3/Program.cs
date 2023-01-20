dba552080f9fe7d00b2c897bf2f378c0cfe46509
Polina <ms.maylin@inbox.ru>
2023-01-21 00:43:05 +0300

    In task3: another solution added in program.cs

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -20,3 +20,24 @@ else
 {
     Console.WriteLine($"Дня недели с номером {dayNumber} нет.");
 }
+
+
+// Второй вариант решения:
+
+// Console.Write("Введите номер дня недели: ");
+// int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+// switch (dayNumber)
+// {
+//     case 6: case 7:
+//         Console.WriteLine($"День недели номер {dayNumber} является выходным.");
+//         break;
+
+//     case 1: case 2: case 3: case 4: case 5:
+//         Console.WriteLine($"День недели номер {dayNumber} не является выходным.");
+//         break;
+
+//     default:
+//         Console.WriteLine($"Дня недели с номером {dayNumber} нет.");
+//         break;
+// }
