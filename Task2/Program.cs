064e3646b15a9d018c108b946162194cd40ba2bd
Polina <ms.maylin@inbox.ru>
2023-01-21 00:11:20 +0300

    Task2 added

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,3 +6,32 @@
 //
 // 32679 -> 6
 
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num;
+
+if (num >= 100)
+{
+    while (num1 >= 1000)
+    {
+        num1 = (num1 / 10);
+    }
+    Console.WriteLine($"Третья цифра числа {num}: {num1 % 10}");
+}
+else
+{
+    Console.WriteLine($"Третьей цифры в числе {num} нет.");
+}
+
+// Второй способ решения:
+
+// Console.Write("Введите число: ");
+// String text = Console.ReadLine();
+// if (text.Length < 3)
+// {
+//     Console.WriteLine($"Третьей цифры в числе {text} нет.");
+// }
+// else
+// {
+//     Console.WriteLine($"Третья цифра числа {text}: {text[2]}");
+// }
